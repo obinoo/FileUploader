f092daf62fdb3277c9d8e7fdf89307a4babab895
obinoo <nnubiaobinna17@outlook.com>
2022-12-13 00:33:54 +0100

    Version of FileUploader

--- src/main/java/com/example/FileUploader/Controller/FileController.java
+++ src/main/java/com/example/FileUploader/Controller/FileController.java
@@ -24,65 +24,18 @@ public class FileController {
     @PostMapping("/upload-image")
     public String uploadImage(@RequestParam("file") MultipartFile file) throws Exception {
         fileService.uploadFile(file);
-        return "File uploaded Successfully" ;
+        return "File uploaded Successfully";
     }
 
 
-     @GetMapping("/links")
+    @GetMapping("/links")
     public Object imageUrl() throws IOException, ExecutionException, InterruptedException {
-         return fileService.imageUrl();
-     }
+        return fileService.imageUrl();
+    }
 
-     @DeleteMapping("/{id}")
-     public void delete(@PathVariable("id") long id){
-         this.id = id;
-         fileService.deleteImage();
-     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    @PostMapping("/upload")
-//    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-//        String message = "";
-//
-//        fileService.store(file);
-//
-//        message = "Uploaded the file successfully: " + file.getOriginalFilename();
-//        return ResponseEntity.status(HttpStatus.OK).body("Success");
-//    }
-//
-//    @GetMapping("/files")
-//    public ResponseEntity<List<ResponseFile>> getListFiles() {
-//        List<ResponseFile> files = fileService.getAllFiles().map(dbFile -> {
-//            String fileDownloadUri = ServletUriComponentsBuilder
-//                    .fromCurrentContextPath()
-//                    .path("/files/")
-//                    .path(dbFile.getId())
-//                    .toUriString();
-//
-//            return new ResponseFile(
-//                    dbFile.getName(),
-//                    fileDownloadUri,
-//                    dbFile.getType());
-//        }).collect(Collectors.toList());
-//
-//        return ResponseEntity.status(HttpStatus.OK).body(files);
-//    }
-
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") long id) {
+        this.id = id;
+        fileService.deleteImage();
+    }
 }
